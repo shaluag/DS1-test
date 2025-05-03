f2b4f803edaa5b5a9bcc9cc239eb1d8d38959355
shaluag <159629027+shaluag@users.noreply.github.com>
2025-05-03 10:56:04 +0530

    Add files via upload

---
+++ sunita.py
@@ -0,0 +1,26 @@
+def add(a,b=0,c=6):
+    return a+b+c
+print(add(5,8))
+
+###########
+def fact(n):
+    total=1
+    for i in range(n):
+        total=total*i
+        return total
+
+<<<<<<< HEAD
+###########333
+def fib(n):
+    if n==0 or n==1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+    
+=======
+##########
+def str(n):
+    for i in range(1,n):
+        print(i*"*")
+         
+>>>>>>> Ashish
